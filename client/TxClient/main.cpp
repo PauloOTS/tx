c7f3e49b40cec335eb6b81f8f31e28a089b4428f
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2017-06-07 08:36:03 -0300

    Fix money evasion aka Box 2
    Fix money evasion aka Box 2 in the client application for account 0

--- client/TxClient/main.cpp
+++ client/TxClient/main.cpp
@@ -2,23 +2,11 @@
 #include <QApplication>
 #include <QDebug>
 
-#include "webservice.hpp"
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	TxClientView w;
 	w.show();
-
-	try{
-		WebService webservice("localhost", 5000);
-
-		Deposit d(0, 10.0, "saving");
-		qDebug() << webservice.deposit(d);
-
-	}catch(WebServiceError& error){
-		qDebug() << error.msg;
-	}
 
 	return a.exec();
 }
